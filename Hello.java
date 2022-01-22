093c77bc0b15f9d28676eb51ab0410bb711eddd6
yara-codes <98223386+yara-codes@users.noreply.github.com>
2022-01-22 09:01:50 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************
+* Lab 01 - Instructions
+************************
+* Yara Cooper
+* 1/19/2022
+* Comp Sci 255
+***************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Yara Cooper");
+		System.out.println("Lab 01 - Instructions");
+		System.out.println("Run Java");
+		System.out.println("1/19/2022");
+		System.out.println("CMSC 255 - C90");
+		System.out.println();
+	
+	}
+}
